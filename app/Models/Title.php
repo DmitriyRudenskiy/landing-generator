832c136295d9be5b58e2d726977a1579bc76a25d
Dmitriy Rudenskiy <dmitriy.rudenskiy@gmail.com>
2017-05-18 13:32:37 +0700

    add: Yandex metrika

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -16,12 +16,15 @@ class Title extends Model
 
     const TITLE_DOWN_BUTTON = 'title_down_button';
 
+    const TITLE_YANDEX_METRIKA = 'metrika';
+
     public static function getKeys()
     {
         return [
             self::TILE_MIME => 1,
             self::TILE_PRODUCT => 2,
-            self::TITLE_DOWN_BUTTON => 3
+            self::TITLE_DOWN_BUTTON => 3,
+            self::TITLE_YANDEX_METRIKA => 5
         ];
     }
 }

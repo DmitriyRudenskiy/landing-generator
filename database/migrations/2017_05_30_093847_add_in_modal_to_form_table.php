af702da6df310ad43c6ef8798aaeca09aa7dc2d2
Dmitriy Rudenskiy <dmitriy.rudenskiy@gmail.com>
2017-05-30 18:01:17 +0700

    add: edit form

---
+++ database/migrations/2017_05_30_093847_add_in_modal_to_form_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddInModalToFormTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('form', function (Blueprint $table) {
+            $table->boolean('in_modal')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('form', function (Blueprint $table) {
+            //
+        });
+    }
+}
